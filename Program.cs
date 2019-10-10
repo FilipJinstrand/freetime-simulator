fb0e2e13c3057984278edf18d13178f2b5bc1cce
Filip Jinstrand <filip@jinstrand.se>
2019-10-10 10:47:46 +0200

    First commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace freetime_simulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
